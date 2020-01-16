3e14dd16ba83ab764ef6698e2dca9a69e624c055
Robert Shields <robert.shieldsiv@gmail.com>
2020-01-16 23:05:59 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require_relative '../config/environment'
 
 Hashie
@@ -5,3 +6,12 @@ Sinatra
 Octokit
 Pry
 AwesomePrint
+=======
+require_relative '../config/environment'
+
+Hashie
+Sinatra
+Octokit
+Pry
+AwesomePrint
+>>>>>>> d8884ae609fc2d51de012577aee7de01ec1d9f7c
